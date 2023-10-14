9b296a50d18a5060d7e577844414d1aafb87df64
BoltLi <2418454228@qq.com>
2023-10-14 16:17:44 +0800

    网站模板

---
+++ 3/bbs.js
@@ -0,0 +1,17 @@
+skins=getCookie('skins')
+
+function bbsxptop(){
+document.write("<table width=800 heigh=144 align=center><tr><td><table cellpadding=0 cellspacing=0 width=760 align=center><tr><td width=100% background=images/skins/"+skins+"/top.gif align=center><embed wmode=transparent src=images/skins/"+skins+"/top.swf width=100% height=285></td></tr></table>")
+}
+
+function bbsxpbottom(){
+document.write("<CENTER><FONT COLOR=#7B8894>[当前风格∶美丽心情]</FONT></CENTER><table cellpadding=0 cellspacing=0 width=760 align=center><tr><td><img src=images/skins/"+skins+"/endlogo.gif></td></tr></table>")
+}
+
+function valigntop(){
+document.write("<table cellpadding=0 cellspacing=0 width=760 align=center><tr><td><img src=images/skins/"+skins+"/T_left.gif></td><td width=100% background=images/skins/"+skins+"/T_bg.gif></td><td><img src=images/skins/"+skins+"/T_right.gif></td></tr></table>")
+}
+
+function valignbottom(){
+
+}
